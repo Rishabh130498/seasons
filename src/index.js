f4332ef9bf1ab37e419658e7e25401203ff0ee21
Rishabh Agarwal <Rishabh130498@gmail.com>
2020-12-13 17:26:34 +0530

    constructor fn, comonentdidmount and render fn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+  render() {
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Lat:{this.state.lat}</div>;
+    }
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>errorMessage:{this.state.errorMessage}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
