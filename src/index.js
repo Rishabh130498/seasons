85e544948aec38c796f6c8402165b5648b7b5e7d
Rishabh Agarwal <Rishabh130498@gmail.com>
2020-12-15 16:28:56 +0530

    Text for winter or summer shows up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -12,7 +13,7 @@ class App extends React.Component {
   }
   render() {
     if (this.state.lat && !this.state.errorMessage) {
-      return <div>Lat:{this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     if (!this.state.lat && this.state.errorMessage) {
       return <div>errorMessage:{this.state.errorMessage}</div>;
